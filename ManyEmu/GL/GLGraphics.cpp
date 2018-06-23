3fbe8e21e7533498f86f5df40b726e672c657963
Antoine Bouchard <boucha05@yahoo.ca>
2018-06-23 15:53:15 -0400

    Fixing naming conventions in ImGui GL backend.

--- ManyEmu/GL/GLGraphics.cpp
+++ ManyEmu/GL/GLGraphics.cpp
@@ -10,17 +10,42 @@ namespace
 {
     struct ImGuiRenderer
     {
-        GLuint       g_FontTexture = 0;
-        int          g_ShaderHandle = 0, g_VertHandle = 0, g_FragHandle = 0;
-        int          g_AttribLocationTex = 0, g_AttribLocationProjMtx = 0;
-        int          g_AttribLocationPosition = 0, g_AttribLocationUV = 0, g_AttribLocationColor = 0;
-        unsigned int g_VboHandle = 0, g_VaoHandle = 0, g_ElementsHandle = 0;
+        GLuint          mFontTexture = 0;
+        GLuint          mShaderHandle = 0;
+        GLuint          mVertHandle = 0;
+        GLuint          mFragHandle = 0;
+        GLuint          mAttribLocationTex = 0;
+        GLuint          mAttribLocationProjMtx = 0;
+        GLuint          mAttribLocationPosition = 0;
+        GLuint          mAttribLocationUV = 0;
+        GLuint          mAttribLocationColor = 0;
+        GLuint          mVboHandle = 0;
+        GLuint          mVaoHandle = 0;
+        GLuint          mElementsHandle = 0;
+        int32_t         mFrameBufferSizeX = 0;
+        int32_t         mFrameBufferSizeY = 0;
+        GLint           mLastProgram;
+        GLint           mLastTexture;
+        GLint           mLastActiveTexture;
+        GLint           mLastArrayBuffer;
+        GLint           mLastElementArrayBuffer;
+        GLint           mLastVertexArray;
+        GLint           mLastBlendSrc;
+        GLint           mLastBlendDst;
+        GLint           mLastBlendEquationRgb;
+        GLint           mLastBlendEquationAlpha;
+        GLint           mLastViewport[4];
+        GLint           mLastScissorBox[4];
+        GLboolean       mLastEnableBlend;
+        GLboolean       mLastEnableCullFace;
+        GLboolean       mLastEnableDepthTest;
+        GLboolean       mLastEnableScissorTest;
 
         bool initialize()
         {
             // Backup GL state
-            glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &last_array_buffer);
-            glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &last_vertex_array);
+            glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &mLastArrayBuffer);
+            glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &mLastVertexArray);
 
             const GLchar *vertex_shader =
                 "#version 330\n"
@@ -48,63 +73,63 @@ namespace
                 "   Out_Color = Frag_Color * texture( Texture, Frag_UV.st);\n"
                 "}\n";
 
-            g_ShaderHandle = glCreateProgram();
-            g_VertHandle = glCreateShader(GL_VERTEX_SHADER);
-            g_FragHandle = glCreateShader(GL_FRAGMENT_SHADER);
-            glShaderSource(g_VertHandle, 1, &vertex_shader, 0);
-            glShaderSource(g_FragHandle, 1, &fragment_shader, 0);
-            glCompileShader(g_VertHandle);
-            glCompileShader(g_FragHandle);
-            glAttachShader(g_ShaderHandle, g_VertHandle);
-            glAttachShader(g_ShaderHandle, g_FragHandle);
-            glLinkProgram(g_ShaderHandle);
+            mShaderHandle = glCreateProgram();
+            mVertHandle = glCreateShader(GL_VERTEX_SHADER);
+            mFragHandle = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(mVertHandle, 1, &vertex_shader, 0);
+            glShaderSource(mFragHandle, 1, &fragment_shader, 0);
+            glCompileShader(mVertHandle);
+            glCompileShader(mFragHandle);
+            glAttachShader(mShaderHandle, mVertHandle);
+            glAttachShader(mShaderHandle, mFragHandle);
+            glLinkProgram(mShaderHandle);
 
-            g_AttribLocationTex = glGetUniformLocation(g_ShaderHandle, "Texture");
-            g_AttribLocationProjMtx = glGetUniformLocation(g_ShaderHandle, "ProjMtx");
-            g_AttribLocationPosition = glGetAttribLocation(g_ShaderHandle, "Position");
-            g_AttribLocationUV = glGetAttribLocation(g_ShaderHandle, "UV");
-            g_AttribLocationColor = glGetAttribLocation(g_ShaderHandle, "Color");
+            mAttribLocationTex = glGetUniformLocation(mShaderHandle, "Texture");
+            mAttribLocationProjMtx = glGetUniformLocation(mShaderHandle, "ProjMtx");
+            mAttribLocationPosition = glGetAttribLocation(mShaderHandle, "Position");
+            mAttribLocationUV = glGetAttribLocation(mShaderHandle, "UV");
+            mAttribLocationColor = glGetAttribLocation(mShaderHandle, "Color");
 
-            glGenBuffers(1, &g_VboHandle);
-            glGenBuffers(1, &g_ElementsHandle);
+            glGenBuffers(1, &mVboHandle);
+            glGenBuffers(1, &mElementsHandle);
 
-            glGenVertexArrays(1, &g_VaoHandle);
-            glBindVertexArray(g_VaoHandle);
-            glBindBuffer(GL_ARRAY_BUFFER, g_VboHandle);
-            glEnableVertexAttribArray(g_AttribLocationPosition);
-            glEnableVertexAttribArray(g_AttribLocationUV);
-            glEnableVertexAttribArray(g_AttribLocationColor);
+            glGenVertexArrays(1, &mVaoHandle);
+            glBindVertexArray(mVaoHandle);
+            glBindBuffer(GL_ARRAY_BUFFER, mVboHandle);
+            glEnableVertexAttribArray(mAttribLocationPosition);
+            glEnableVertexAttribArray(mAttribLocationUV);
+            glEnableVertexAttribArray(mAttribLocationColor);
 
 #define OFFSETOF(TYPE, ELEMENT) ((size_t)&(((TYPE *)0)->ELEMENT))
-            glVertexAttribPointer(g_AttribLocationPosition, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, pos));
-            glVertexAttribPointer(g_AttribLocationUV, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, uv));
-            glVertexAttribPointer(g_AttribLocationColor, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, col));
+            glVertexAttribPointer(mAttribLocationPosition, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, pos));
+            glVertexAttribPointer(mAttribLocationUV, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, uv));
+            glVertexAttribPointer(mAttribLocationColor, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, col));
 #undef OFFSETOF
 
             // Restore modified GL state
-            glBindBuffer(GL_ARRAY_BUFFER, last_array_buffer);
-            glBindVertexArray(last_vertex_array);
+            glBindBuffer(GL_ARRAY_BUFFER, mLastArrayBuffer);
+            glBindVertexArray(mLastVertexArray);
 
             return true;
         }
 
         ~ImGuiRenderer()
         {
-            if (g_VaoHandle) glDeleteVertexArrays(1, &g_VaoHandle);
-            if (g_VboHandle) glDeleteBuffers(1, &g_VboHandle);
-            if (g_ElementsHandle) glDeleteBuffers(1, &g_ElementsHandle);
-            g_VaoHandle = g_VboHandle = g_ElementsHandle = 0;
+            if (mVaoHandle) glDeleteVertexArrays(1, &mVaoHandle);
+            if (mVboHandle) glDeleteBuffers(1, &mVboHandle);
+            if (mElementsHandle) glDeleteBuffers(1, &mElementsHandle);
+            mVaoHandle = mVboHandle = mElementsHandle = 0;
 
-            if (g_ShaderHandle && g_VertHandle) glDetachShader(g_ShaderHandle, g_VertHandle);
-            if (g_VertHandle) glDeleteShader(g_VertHandle);
-            g_VertHandle = 0;
+            if (mShaderHandle && mVertHandle) glDetachShader(mShaderHandle, mVertHandle);
+            if (mVertHandle) glDeleteShader(mVertHandle);
+            mVertHandle = 0;
 
-            if (g_ShaderHandle && g_FragHandle) glDetachShader(g_ShaderHandle, g_FragHandle);
-            if (g_FragHandle) glDeleteShader(g_FragHandle);
-            g_FragHandle = 0;
+            if (mShaderHandle && mFragHandle) glDetachShader(mShaderHandle, mFragHandle);
+            if (mFragHandle) glDeleteShader(mFragHandle);
+            mFragHandle = 0;
 
-            if (g_ShaderHandle) glDeleteProgram(g_ShaderHandle);
-            g_ShaderHandle = 0;
+            if (mShaderHandle) glDeleteProgram(mShaderHandle);
+            mShaderHandle = 0;
 
             destroyFontTexture();
         }
@@ -112,63 +137,44 @@ namespace
         bool createFontTexture(const ImGuiRenderer_SetFontTexture_Params& params)
         {
             // Upload texture to graphics system
-            glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
-            glGenTextures(1, &g_FontTexture);
-            glBindTexture(GL_TEXTURE_2D, g_FontTexture);
+            glGetIntegerv(GL_TEXTURE_BINDING_2D, &mLastTexture);
+            glGenTextures(1, &mFontTexture);
+            glBindTexture(GL_TEXTURE_2D, mFontTexture);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
             glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, params.width, params.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, params.pixels);
 
             // Restore state
-            glBindTexture(GL_TEXTURE_2D, last_texture);
+            glBindTexture(GL_TEXTURE_2D, mLastTexture);
 
             return true;
         }
 
         void destroyFontTexture()
         {
-            if (g_FontTexture)
+            if (mFontTexture)
             {
-                glDeleteTextures(1, &g_FontTexture);
-                g_FontTexture = 0;
+                glDeleteTextures(1, &mFontTexture);
+                mFontTexture = 0;
             }
         }
-
-        GLint last_program;
-        GLint last_texture;
-        GLint last_active_texture;
-        GLint last_array_buffer;
-        GLint last_element_array_buffer;
-        GLint last_vertex_array;
-        GLint last_blend_src;
-        GLint last_blend_dst;
-        GLint last_blend_equation_rgb;
-        GLint last_blend_equation_alpha;
-        GLint last_viewport[4];
-        GLint last_scissor_box[4];
-        GLboolean last_enable_blend;
-        GLboolean last_enable_cull_face;
-        GLboolean last_enable_depth_test;
-        GLboolean last_enable_scissor_test;
-        int32_t mFrameBufferSizeX = 0;
-        int32_t mFrameBufferSizeY = 0;
 
         void beginDraw(const ImGuiRenderer_BeginDraw_Params& params)
         {
             // Backup GL state
-            glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
-            glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
-            glGetIntegerv(GL_ACTIVE_TEXTURE, &last_active_texture);
-            glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &last_array_buffer);
-            glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, &last_element_array_buffer);
-            glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &last_vertex_array);
-            glGetIntegerv(GL_BLEND_SRC, &last_blend_src);
-            glGetIntegerv(GL_BLEND_DST, &last_blend_dst);
-            glGetIntegerv(GL_BLEND_EQUATION_RGB, &last_blend_equation_rgb);
-            glGetIntegerv(GL_BLEND_EQUATION_ALPHA, &last_blend_equation_alpha);
-            glGetIntegerv(GL_VIEWPORT, last_viewport);
-            glGetIntegerv(GL_SCISSOR_BOX, last_scissor_box);
+            glGetIntegerv(GL_CURRENT_PROGRAM, &mLastProgram);
+            glGetIntegerv(GL_TEXTURE_BINDING_2D, &mLastTexture);
+            glGetIntegerv(GL_ACTIVE_TEXTURE, &mLastActiveTexture);
+            glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &mLastArrayBuffer);
+            glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, &mLastElementArrayBuffer);
+            glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &mLastVertexArray);
+            glGetIntegerv(GL_BLEND_SRC, &mLastBlendSrc);
+            glGetIntegerv(GL_BLEND_DST, &mLastBlendDst);
+            glGetIntegerv(GL_BLEND_EQUATION_RGB, &mLastBlendEquationRgb);
+            glGetIntegerv(GL_BLEND_EQUATION_ALPHA, &mLastBlendEquationAlpha);
+            glGetIntegerv(GL_VIEWPORT, mLastViewport);
+            glGetIntegerv(GL_SCISSOR_BOX, mLastScissorBox);
             glIsEnabled(GL_BLEND);
             glIsEnabled(GL_CULL_FACE);
             glIsEnabled(GL_DEPTH_TEST);
@@ -194,15 +200,15 @@ namespace
                 { 0.0f,                     0.0f,                     -1.0f, 0.0f },
                 {-1.0f,                     1.0f,                      0.0f, 1.0f },
             };
-            glUseProgram(g_ShaderHandle);
-            glUniform1i(g_AttribLocationTex, 0);
-            glUniformMatrix4fv(g_AttribLocationProjMtx, 1, GL_FALSE, &ortho_projection[0][0]);
-            glBindVertexArray(g_VaoHandle);
+            glUseProgram(mShaderHandle);
+            glUniform1i(mAttribLocationTex, 0);
+            glUniformMatrix4fv(mAttribLocationProjMtx, 1, GL_FALSE, &ortho_projection[0][0]);
+            glBindVertexArray(mVaoHandle);
 
-            glBindBuffer(GL_ARRAY_BUFFER, g_VboHandle);
+            glBindBuffer(GL_ARRAY_BUFFER, mVboHandle);
             glBufferData(GL_ARRAY_BUFFER, (GLsizeiptr)params.vertexCount * sizeof(ImDrawVert), (GLvoid*)params.vertexData, GL_STREAM_DRAW);
 
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ElementsHandle);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mElementsHandle);
             glBufferData(GL_ELEMENT_ARRAY_BUFFER, (GLsizeiptr)params.indexCount * sizeof(ImDrawIdx), (GLvoid*)params.indexData, GL_STREAM_DRAW);
         }
 
@@ -216,20 +222,20 @@ namespace
         void endDraw()
         {
             // Restore modified GL state
-            glUseProgram(last_program);
-            glActiveTexture(last_active_texture);
-            glBindTexture(GL_TEXTURE_2D, last_texture);
-            glBindVertexArray(last_vertex_array);
-            glBindBuffer(GL_ARRAY_BUFFER, last_array_buffer);
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, last_element_array_buffer);
-            glBlendEquationSeparate(last_blend_equation_rgb, last_blend_equation_alpha);
-            glBlendFunc(last_blend_src, last_blend_dst);
-            if (last_enable_blend) glEnable(GL_BLEND); else glDisable(GL_BLEND);
-            if (last_enable_cull_face) glEnable(GL_CULL_FACE); else glDisable(GL_CULL_FACE);
-            if (last_enable_depth_test) glEnable(GL_DEPTH_TEST); else glDisable(GL_DEPTH_TEST);
-            if (last_enable_scissor_test) glEnable(GL_SCISSOR_TEST); else glDisable(GL_SCISSOR_TEST);
-            glViewport(last_viewport[0], last_viewport[1], (GLsizei)last_viewport[2], (GLsizei)last_viewport[3]);
-            glScissor(last_scissor_box[0], last_scissor_box[1], (GLsizei)last_scissor_box[2], (GLsizei)last_scissor_box[3]);
+            glUseProgram(mLastProgram);
+            glActiveTexture(mLastActiveTexture);
+            glBindTexture(GL_TEXTURE_2D, mLastTexture);
+            glBindVertexArray(mLastVertexArray);
+            glBindBuffer(GL_ARRAY_BUFFER, mLastArrayBuffer);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mLastElementArrayBuffer);
+            glBlendEquationSeparate(mLastBlendEquationRgb, mLastBlendEquationAlpha);
+            glBlendFunc(mLastBlendSrc, mLastBlendDst);
+            if (mLastEnableBlend) glEnable(GL_BLEND); else glDisable(GL_BLEND);
+            if (mLastEnableCullFace) glEnable(GL_CULL_FACE); else glDisable(GL_CULL_FACE);
+            if (mLastEnableDepthTest) glEnable(GL_DEPTH_TEST); else glDisable(GL_DEPTH_TEST);
+            if (mLastEnableScissorTest) glEnable(GL_SCISSOR_TEST); else glDisable(GL_SCISSOR_TEST);
+            glViewport(mLastViewport[0], mLastViewport[1], (GLsizei)mLastViewport[2], (GLsizei)mLastViewport[3]);
+            glScissor(mLastScissorBox[0], mLastScissorBox[1], (GLsizei)mLastScissorBox[2], (GLsizei)mLastScissorBox[3]);
         }
     };
 
@@ -260,7 +266,7 @@ namespace
 
         void* imGuiRenderer_getFontTexture() override
         {
-            return mImGuiRenderer ? (void *)(intptr_t)mImGuiRenderer->g_FontTexture : nullptr;
+            return mImGuiRenderer ? (void *)(intptr_t)mImGuiRenderer->mFontTexture : nullptr;
         }
 
         bool imGuiRenderer_beginDraw(const ImGuiRenderer_BeginDraw_Params& params) override
